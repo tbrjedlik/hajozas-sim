3ff0f4e538da7edb80d4d6a8badd498f7ee9a8b2
toth.barna.robert <toth.barna.robert@C103-15>
2025-02-07 09:35:22 +0100

    Ship class

---
+++ Hajozas Sim/Hajozas Sim/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hajozas_Sim
+{
+    public class Ship
+    {
+        public string ShipId { get; set; }
+        public string ShipType { get; set; }
+        public double Capacity { get; set; }
+        public string CurrentStatus { get; set; }
+        public (double x, double y) CurrentPosition { get; set; }
+        public List<Cargo> ShipCargo { get; set; }
+
+        public Ship(string shipId, string shipType, double capacity)
+        {
+            ShipId = shipId;
+            ShipType = shipType;
+            Capacity = capacity;
+            CurrentStatus = "Docked";
+            CurrentPosition = (0, 0);
+            ShipCargo = new List<Cargo>();
+        }
+
+        public void Depart()
+        {
+            CurrentStatus = "Sailing";
+        }
+
+        public void Arrive()
+        {
+            CurrentStatus = "Docked";
+        }
+    }
+}
