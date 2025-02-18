fc44efa1e6103982f61c31d72095e5e354c2182f
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-02-18 15:19:40 +0100

    Ship.cs szintaktika jav

--- Hajozas Sim/Hajozas Sim/Ship.cs
+++ Hajozas Sim/Hajozas Sim/Ship.cs
@@ -7,58 +7,59 @@ using System.Threading.Tasks;
 namespace Hajozas_Sim
 {
     public class Ship
-{
-    public string ShipId { get; set; }
-    public string ShipType { get; set; }
-    public double Capacity { get; set; }
-    public double Speed { get; set; }
-    public string CurrentStatus { get; set; }
-    public (double x, double y) CurrentPosition { get; set; }
-    public List<Cargo> ShipCargo { get; set; }
-
-    public Ship(string shipId, string shipType, double capacity, double speed)
     {
-        ShipId = shipId;
-        ShipType = shipType;
-        Capacity = capacity;
-        Speed = speed;
-        CurrentStatus = "Docked";
-        CurrentPosition = (0, 0);
-        ShipCargo = new List<Cargo>();
-    }
+        public string ShipId { get; set; }
+        public string ShipType { get; set; }
+        public double Capacity { get; set; }
+        public double Speed { get; set; }
+        public string CurrentStatus { get; set; }
+        public (double x, double y) CurrentPosition { get; set; }
+        public List<Cargo> ShipCargo { get; set; }
 
-    public void Depart()
-    {
-        CurrentStatus = "Sailing";
-    }
-
-    public void Arrive()
-    {
-        CurrentStatus = "Docked";
-    }
-
-    public void LoadCargo(Cargo cargo)
-    {
-        if (cargo == null)
-            throw new ArgumentNullException(nameof(cargo), "A rakomány nem lehet null.");
-
-        double totalWeight = 0;
-        foreach (var c in ShipCargo)
+        public Ship(string shipId, string shipType, double capacity, double speed)
         {
-            totalWeight += c.Weight;
+            ShipId = shipId;
+            ShipType = shipType;
+            Capacity = capacity;
+            Speed = speed;
+            CurrentStatus = "Docked";
+            CurrentPosition = (0, 0);
+            ShipCargo = new List<Cargo>();
         }
 
-        if (totalWeight + cargo.Weight > Capacity)
-            throw new InvalidOperationException("A hajó nem bír el ennyi rakományt.");
+        public void Depart()
+        {
+            CurrentStatus = "Sailing";
+        }
 
-        ShipCargo.Add(cargo);
-    }
+        public void Arrive()
+        {
+            CurrentStatus = "Docked";
+        }
 
-    public void UnloadCargo(Cargo cargo)
-    {
-        if (cargo == null)
-            throw new ArgumentNullException(nameof(cargo), "A rakomány nem lehet null.");
+        public void LoadCargo(Cargo cargo)
+        {
+            if (cargo == null)
+                throw new ArgumentNullException(nameof(cargo), "A rakomány nem lehet null.");
 
-        ShipCargo.Remove(cargo);
+            double totalWeight = 0;
+            foreach (var c in ShipCargo)
+            {
+                totalWeight += c.Weight;
+            }
+
+            if (totalWeight + cargo.Weight > Capacity)
+                throw new InvalidOperationException("A hajó nem bír el ennyi rakományt.");
+
+            ShipCargo.Add(cargo);
+        }
+
+        public void UnloadCargo(Cargo cargo)
+        {
+            if (cargo == null)
+                throw new ArgumentNullException(nameof(cargo), "A rakomány nem lehet null.");
+
+            ShipCargo.Remove(cargo);
+        }
     }
 }
